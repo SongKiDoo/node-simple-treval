e3dbe9a5d0a5c43a6792f88727d6e3d7c69b433f
sohyun <ksi7777760@naver.com>
2018-12-09 23:16:51 +0900

    - 메인페이지 course, thema 데이터 가져오기

--- models/review_comment.js
+++ models/review_comment.js
@@ -31,7 +31,7 @@ module.exports = function(sequelize, DataTypes) {
 			type: DataTypes.INTEGER(11),
 			allowNull: true
 		},
-		comment_disc: {
+		comment_dics: {
 			type: DataTypes.INTEGER(11),
 			allowNull: true
 		},
