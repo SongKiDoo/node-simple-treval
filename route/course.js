1f27dd2ee7860d3a087f4695e7348ad3bcf0dc6e
KiDooSong <only3ss@smartcon-metaps.com>
2018-12-13 21:27:29 +0900

    [modify] 코스,테마 리스트 추가 / 메인페이지 개수 3개씩으로 변경

--- route/course.js
+++ route/course.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var router = express.Router();
+let models = require('../models/database');
 
 router.get('/', function (req, res) {
     const sess = req.session;
 
-    res.render('course.html', {
-        sess: sess
+    const customQuery = { course_id : id };
+
+    models.travel_place.findAll({where : customQuery}).then(function (result) {
+        return res.render('course.html', {
+            sess: sess,
+            courseList: result
+        });
     });
 });
 
--- route/thema.js
+++ route/thema.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var router = express.Router();
+let models = require('../models/database');
 
 router.get('/', function (req, res) {
     const sess = req.session;
 
-    res.render('thema.html', {
-        sess: sess
+    const customQuery = { theme_id : id };
+
+    models.travel_place.findAll({where : customQuery}).then(function (result) {
+        res.render('thema.html', {
+            sess: sess,
+            themeList: result
+        });
     });
 });
 
